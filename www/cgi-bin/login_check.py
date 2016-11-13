6715b08892a9f4f8728755a67e83e09417cbdb9a
tdias2 <TiF@dhcp-10-4-19-113.wireless.rochester.edu>
2016-11-12 19:56:38 -0500

    Cookie updates

--- www/cgi-bin/login_check.py
+++ www/cgi-bin/login_check.py
@@ -7,13 +7,14 @@
 #Import stuff
 #sqlite3 used for ease of versioning over Mysql
 import cgitb
-import cgi
-import hashlib
-import sqlite3
+
 cgitb.enable()
 
 #authenticate that this user's password and username match what is present in the database
 def authenticate(username,password):
+    import hashlib
+    import sqlite3
+
     conn = sqlite3.connect('weatherwindow.db')
     #conn.row_factory = lambda cursor, row: row[0]
     cursor = conn.cursor()
@@ -54,53 +55,96 @@ def authenticate(username,password):
 
         return False
 
+import Cookie
+import os
 
-
-
-
+import cgi
 login_form = cgi.FieldStorage()
 
-print ('Content-Type: text/html\n\n')
-print ()
-
-print ('''<html>
-    <head>
-        <title>Login Results</title>
-    </head>
-    <body>''')
-
 if "usernamefield" and "passwordfield" not in login_form:
+    print ('Content-Type: text/html\n\n')
+    print ()
+    print ('''<html>
+        <head>
+            <title>Login Results</title>
+        </head>
+        <body>''')
     print ('<h1>Please return to the login page and provide a username and password </h1>')
+    print ('''
+        </body>
+    </html>''')
 elif "usernamefield"  not in login_form:
+    print ('Content-Type: text/html\n\n')
+    print ()
+    print ('''<html>
+        <head>
+            <title>Login Results</title>
+        </head>
+        <body>''')
     print ('<h1>Please return to the login page and provide a username</h1>')
+    print ('''
+        </body>
+    </html>''')
 elif "passwordfield" not in login_form:
+    print ('Content-Type: text/html\n\n')
+    print ()
+    print ('''<html>
+        <head>
+            <title>Login Results</title>
+        </head>
+        <body>''')
     print ('<h1>Please return to the login page and provide a password </h1>')
+    print ('''
+        </body>
+    </html>''')
 else:
     username = login_form.getvalue('usernamefield')
     password = login_form.getvalue('passwordfield')
 
+    #Create cookie
+    c = Cookie.SimpleCookie()
+    c['username'] = username
+    c['username']['path'] = '/'
+    c['username']['expires'] = 30 * 24 * 60 * 60
+
     pizza = authenticate(username,password)
 
-
-    print('<h1> output', pizza,' </h1>')
+    #print('<h1> output', pizza,' </h1>')
 
     if pizza==2:
+        print "Content-Type: text/html"
+        #send cookie
+        print c
+        print
         print ('<h1>User ' , username , ' has been successfully authenticated!</h1>')
         #redirect to welcome page
         print '<META HTTP-EQUIV=refresh CONTENT=\"1;URL=/MainScreen.html\">\n'
         print
+        print ('''
+            </body>
+        </html>''')
     elif pizza == 3:
+        print "Content-Type: text/html"
+        print
         print ('<h1>Authentication Failed for username', username, '! </h1>')
         print '<META HTTP-EQUIV=refresh CONTENT=\"1;URL=/FailedLogin.html\">\n'
         print
+        print ('''
+            </body>
+        </html>''')
     elif pizza ==1:
+        print "Content-Type: text/html"
+        print
         print ('<h1>No such username', username, 'exists. Please go to signup page. </h1>')
         print '<META HTTP-EQUIV=refresh CONTENT=\"1;URL=/FailedLogin.html\">\n'
+        print ('''
+            </body>
+        </html>''')
     else:
+        print "Content-Type: text/html"
+        print
         print ('<h1>What happened?! </h1>')
         print '<META HTTP-EQUIV=refresh CONTENT=\"1;URL=/FailedLogin.html\">\n'
-
-
-print ('''
-    </body>
-</html>''')
+        print ('''
+            </body>
+        </html>''')
---
+++ www/cgi-bin/logout.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import cgitb
+
+cgitb.enable()
+
+import Cookie
+import os
+
+import cgi
+login_form = cgi.FieldStorage()
+
+stored_cookie_string = os.environ.get('HTTP_COOKIE')
+c = Cookie.SimpleCookie(stored_cookie_string)
+
+nc = Cookie.SimpleCookie()
+nc['username'] = c['username']
+nc['username']['path'] = '/'
+nc['username']['expires'] = -1 * 30 * 24 * 60 * 60
+
+print "Content-Type: text/html"
+print nc
+print
+print "<html>"
+try:
+    data = c['username'].value
+    print "cookie data: " +data+"<br>"
+except KeyError:
+    print "The cookie was not set or has expired<br>"
+print '''	<head>
+		<title>Logout</title>
+		<h1 id="Logout-header">
+			You are logged out!
+		</h1>
+    "</body></html>"
+
+	</head>
+	<br><br>
+	<body>
+            <META HTTP-EQUIV=refresh CONTENT=\"1;URL=/main.html\">
+		</body>
+</html> '''
+print
