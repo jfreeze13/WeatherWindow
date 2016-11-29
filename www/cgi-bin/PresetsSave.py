d0edf3a7b8c7de24e10e1dda4e96ebd05b094db0
jfreeze13 <jfreeze95@yahoo.com>
2016-11-29 02:13:11 -0500

    presets stuff
    Almost everything up and running, trying to work out a DOM issue, but
    cookies are being updated and transmitted and saved and selected from
    server.

--- www/cgi-bin/PresetsSave.py
+++ www/cgi-bin/PresetsSave.py
@@ -12,6 +12,8 @@ import cgi
 import hashlib
 import sqlite3
 
+cgitb.enable()
+
 nc = Cookie.SimpleCookie(os.environ.get('HTTP_COOKIE'))
 username = nc['username'].value
 presetNum = nc['presNum'].value
@@ -32,32 +34,31 @@ if chck =='checked':
         cursor.execute("UPDATE users SET curback=?, curover=? WHERE username=?", (ssn,wthr,username))
 
     elif presetNum == '1':
-        print()
         cursor.execute("UPDATE users SET back1=?, over1=? WHERE username=?", (ssn, wthr, username))
-
+        print()
     elif presetNum == '2':
-        print(presetNum)
+        print()
         cursor.execute("UPDATE users SET back2=?, over2=? WHERE username=?", (ssn, wthr, username))
 
     elif presetNum == '3':
-
         cursor.execute("UPDATE users SET back3=?, over3=? WHERE username=?", (ssn, wthr, username))
-
+        print()
     elif presetNum == '4':
 
         cursor.execute("UPDATE users SET back4=?, over4=? WHERE username=?", (ssn, wthr, username))
-
+        print()
     else:
         print("problem in update")
+        print()
 
 
-    cursor.execute("SELECT curover FROM users WHERE username=?", [username])
-    user =str(cursor.fetchone()[0])
-    print(user)
-    print(presetNum)
-    print(ssn)
-    print(wthr)
-    print()
+    #cursor.execute("SELECT curover FROM users WHERE username=?", [username])
+    #user =str(cursor.fetchone()[0])
+    #print(user)
+    #print(presetNum)
+    #print(ssn)
+    #print(wthr)
+    #print()
 
 else:
 
@@ -69,46 +70,50 @@ else:
         userback = str(cursor.fetchone()[0])
         c = Cookie.SimpleCookie(os.environ['HTTP_COOKIE'])
         c['ssn']['Path']='/'
-        c['ssn']='userback'
+        c['ssn']=userback
         c['wthr']['Path'] = '/'
-        c['wthr'] = 'user'
+        c['wthr'] = user
         print(c)
         print()
     elif presetNum == '2':
-        print(presetNum)
-        print()
         cursor.execute("SELECT over2 FROM users WHERE username=?", [username])
         user = str(cursor.fetchone()[0])
         cursor.execute("SELECT back2 FROM users WHERE username=?", [username])
         userback = str(cursor.fetchone()[0])
-
-    elif presetNum == '3':
-        print(presetNum)
+        c2 = Cookie.SimpleCookie(os.environ['HTTP_COOKIE'])
+        c2['ssn']['Path'] = '/'
+        c2['ssn'] = userback
+        c2['wthr']['Path'] = '/'
+        c2['wthr'] = user
+        print(c2)
         print()
+    elif presetNum == '3':
         cursor.execute("SELECT over3 FROM users WHERE username=?", [username])
         user = str(cursor.fetchone()[0])
         cursor.execute("SELECT back3 FROM users WHERE username=?", [username])
         userback = str(cursor.fetchone()[0])
-
-    elif presetNum == '4':
-        print(presetNum)
+        c3 = Cookie.SimpleCookie(os.environ['HTTP_COOKIE'])
+        c3['ssn']['Path'] = '/'
+        c3['ssn'] = userback
+        c3['wthr']['Path'] = '/'
+        c3['wthr'] = user
+        print(c3)
         print()
+    elif presetNum == '4':
         cursor.execute("SELECT over4 FROM users WHERE username=?", [username])
         user = str(cursor.fetchone()[0])
         cursor.execute("SELECT back4 FROM users WHERE username=?", [username])
         userback = str(cursor.fetchone()[0])
-
+        c4 = Cookie.SimpleCookie(os.environ['HTTP_COOKIE'])
+        c4['ssn']['Path'] = '/'
+        c4['ssn'] = userback
+        c4['wthr']['Path'] = '/'
+        c4['wthr'] = user
+        print(c4)
+        print()
     else:
         print("problem in update")
         print()
-
-
-
-    #print(user)
-    #print(userback)
-    #print(presetNum)
-    #print()
-
 
 conn.commit()
 conn.close()
