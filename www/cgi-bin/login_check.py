ae325cd8073b964af806d38b31a775416fbdc7c2
jigowa <jien.ogawa@gmail.com>
2016-12-06 17:01:41 -0500

    Mac shit

--- www/cgi-bin/login_check.py
+++ www/cgi-bin/login_check.py
@@ -1,4 +1,3 @@
-#!"C:\Program Files (x86)\Ampps\python\python.exe"
 #!/usr/bin/env python
 
 #Author: Jessica Freeze
@@ -7,7 +6,7 @@
 
 #import _mysql
 import cgitb
-import http.cookies as Cookie
+import Cookie
 import os
 import cgi
 import hashlib
@@ -61,41 +60,41 @@ def authenticate(username,password):
 login_form = cgi.FieldStorage()
 
 if "usernamefield" and "passwordfield" not in login_form:
-    print ('Content-Type: text/html\n\n')
-    print ()
-    print ('''<html>
+    print 'Content-Type: text/html\n\n'
+    print 
+    print '''<html>
         <head>
             <title>Login Results</title>
         </head>
-        <body>''')
-    print ('<h1>Please return to the login page and provide a username and password </h1>')
-    print ('''
+        <body>'''
+    print '<h1>Please return to the login page and provide a username and password </h1>'
+    print '''
         </body>
-    </html>''')
+    </html>'''
 elif "usernamefield"  not in login_form:
-    print ('Content-Type: text/html\n\n')
-    print ()
-    print ('''<html>
+    print 'Content-Type: text/html\n\n'
+    print 
+    print '''<html>
         <head>
             <title>Login Results</title>
         </head>
-        <body>''')
-    print ('<h1>Please return to the login page and provide a username</h1>')
-    print ('''
+        <body>'''
+    print '<h1>Please return to the login page and provide a username</h1>'
+    print '''
         </body>
-    </html>''')
+    </html>'''
 elif "passwordfield" not in login_form:
-    print ('Content-Type: text/html\n\n')
-    print ()
-    print ('''<html>
+    print 'Content-Type: text/html\n\n'
+    print 
+    print '''<html>
         <head>
             <title>Login Results</title>
         </head>
-        <body>''')
-    print ('<h1>Please return to the login page and provide a password </h1>')
-    print ('''
+        <body>'''
+    print '<h1>Please return to the login page and provide a password </h1>'
+    print '''
         </body>
-    </html>''')
+    </html>'''
 else:
     username = login_form.getvalue('usernamefield')
     password = login_form.getvalue('passwordfield')
@@ -112,22 +111,22 @@ else:
 
     if pizza==2:
         #send cookie
-        print (c)
-        print ("Status: 301 Moved")
-        print ("Location:/MainScreen.html")
-        print ()
+        print c
+        print "Status: 301 Moved"
+        print "Location:/MainScreen.html"
+        print 
 
     elif pizza == 3:
-        print ("Status: 301 Moved")
-        print ("Location:/FailedLogin.html")
-        print ()
+        print "Status: 301 Moved"
+        print "Location:/FailedLogin.html"
+        print 
     elif pizza ==1:
-        print ("Status: 301 Moved")
-        print ("Location:/FailedLogin.html")
-        print ()
+        print "Status: 301 Moved"
+        print "Location:/FailedLogin.html"
+        print 
     else:
-        print ("Status: 301 Moved")
-        print ("Location:/FailedLogin.html")
-        print ()
+        print "Status: 301 Moved"
+        print "Location:/FailedLogin.html"
+        print 
 
 
